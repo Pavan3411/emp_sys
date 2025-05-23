428e723942d987d996a6b688a8cf7f312d9d2313
Pavan Suthar <71624280+Pavan3411@users.noreply.github.com>
2025-05-23 14:55:07 +0530

    Update employeeModel.js

--- server/models/employeeModel.js
+++ server/models/employeeModel.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose')
+// models/Employee.js
+const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  role: { type: String, required: true },
   doj: { type: Date, required: true },
-  salary: { type: String, required: true },
-  experience: { type: String, required: true },
+  salary: { type: Number, required: true },
+  experience: { type: Number, required: true },
   age: { type: Number, required: true, min: 18 },
   department: { type: String, required: true },
-  location: { type: String },
-});
+  location: { type: String, required: true }
+}, { timestamps: true });
 
-
-module.exports = mongoose.model('Employee', employeeSchema)
+module.exports = mongoose.model('Employee', employeeSchema);
