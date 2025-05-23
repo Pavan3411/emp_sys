4b2a0174004576be9c53a32c0d4a7c4da5cb14c0
Pavan Suthar <71624280+Pavan3411@users.noreply.github.com>
2025-05-23 14:57:03 +0530

    Update employeeRoutes.js

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const allowedRoles = require('../middlewares/allowedRoles');
-const { getEmployeeById, updateEmployee } = require('../controllers/employeeController');
+const { getEmployeeById } = require('../controllers/employeeController');
 
 // Employee can only view their own profile
 router.get('/:id', authMiddleware, allowedRoles('employee'), getEmployeeById);
 
-// Employee can update their own profile
-router.put('/:id', authMiddleware, allowedRoles('employee'), updateEmployee);
 
 module.exports = router;
